1bd91927f650c5b37e2fb39069ba313e313703cf
Yantasticc <yashkalamkhede2002@gmail.com>
2024-01-14 11:26:19 +0530

    get single user  || delete user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -33,4 +33,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+// get single user
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const singleUser = await User.findById({_id : id});
+        res.status(200).json(singleUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// delete operation
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const singleUser = await User.findByIdAndDelete({_id : id});
+        res.status(200).json(singleUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 module.exports = router;
