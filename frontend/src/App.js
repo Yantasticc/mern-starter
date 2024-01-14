35829c81000e0937cca1584be1ec517f75c27858
Yantasticc <yashkalamkhede2002@gmail.com>
2024-01-14 16:46:28 +0530

    routing || form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/Create';
+import Navbar from './components/Navbar';
+import Update from './components/Update';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Read from './components/Read';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Create />} />
+            <Route exact path='/all' element={<Read />} />
+            <Route exact path='/update' element={<Update/>} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
